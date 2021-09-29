bce8437b71d46663b33b66a942e8a58d8be57813
Jakub Kotrs <jakub.kotrs@emplifi.io>
2021-09-29 15:18:53 +0200

    split

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
+import IdleApp from './IdleApp'
+import TowerDefenseApp from './TowerDefenseApp'
+import UiApp from './UiApp'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} width="100" alt="logo" /> Brand assault
       </header>
+      <div style={{display: 'flex'}}>
+        <div>
+
+        <IdleApp/>
+        <UiApp/>
+        </div>
+        <div>
+        <TowerDefenseApp/>
+        </div>
+      </div>
     </div>
   );
 }
