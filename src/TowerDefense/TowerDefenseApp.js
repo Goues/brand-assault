df8bc35def0b2f9151a778ea6cf6d38a0940946b
Ales Menzel <ales.menzel@socialbakers.com>
2021-09-29 16:28:41 +0200

    td: tilemap update

--- src/TowerDefense/TowerDefenseApp.js
+++ src/TowerDefense/TowerDefenseApp.js
@@ -11,31 +11,52 @@ function getTile (resources, type) {
   return new PIXI.Sprite(resources.grass.texture)
 }
 
-// Render background tiles
-app.loader
-.add('grass', '/grass_tile.jpg')
-.add('road', '/road_tile.jpg')
-.load((loader, resources) => {
-  for (let i = 0; i < TILES_X; i++) {
-    for (let j = 0; j < TILES_Y; j++) {
-      const tileType = MAP_1[j][i]
-      const tile = getTile(resources, tileType);
-      tile.x = i * TILE_WIDTH
-      tile.y = j * TILE_HEIGHT
-      tile.width = TILE_WIDTH
-      tile.height = TILE_HEIGHT
+function mountPixi (el) {
+  // Render background tiles
+  app.loader
+  .add('grass', '/grass_tile.jpg')
+  .add('road', '/road_tile.jpg')
+  .load((loader, resources) => {
+    for (let i = 0; i < TILES_X; i++) {
+      for (let j = 0; j < TILES_Y; j++) {
+        const tileType = MAP_1[j][i]
+        const tile = getTile(resources, tileType);
+        tile.x = i * TILE_WIDTH
+        tile.y = j * TILE_HEIGHT
+        tile.width = TILE_WIDTH
+        tile.height = TILE_HEIGHT
 
-      app.stage.addChild(tile);
+        if (tileType === '-') {
+          tile.interactive = true
+          tile.buttonMode = true
+          tile.on('pointerdown', (e) => {
+            alert('clicked!' + e.target.x + ' ' + e.target.y)
+
+
+          });
+        }
+
+        app.stage.addChild(tile);
+      }
     }
-  }
-})
+  })
+
+  el.appendChild(app.view)
+}
 
 export default function TowerDefenseApp() {
   const ref = useRef(null)
   useEffect(() => {
+    const el = ref.current
     // The application will create a canvas element for you that you
     // can then insert into the DOM.
-    ref.current.appendChild(app.view);
+    mountPixi(el)
+
+    return () => {
+      for (const node of [...el.childNodes]) {
+        node.remove()
+      }
+    }
   }, [])
 
   return <div ref={ref}></div>
