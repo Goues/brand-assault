6e371cce6c20571e174cce4968dd54d71fd3267a
Jakub Kotrs <jakub.kotrs@emplifi.io>
2021-09-30 13:44:35 +0200

    Improve rendering performance

--- src/TowerDefense/Hitpoints.js
+++ src/TowerDefense/Hitpoints.js
@@ -25,14 +25,12 @@ class Hitpoints extends PIXI.Container {
     this.text.y = -20;
 
     this.bar = new PIXI.Graphics();
-    this.addChild(this.bar);
-    this.addChild(this.text);
+    this.status = new PIXI.Graphics();
+
+    this.mount();
   }
 
-  draw(hp, hpPercent) {
-    this.text.text = toFixedRound(hp, 0);
-
-    this.bar.clear();
+  mount() {
     this.bar.beginFill(0xffffff);
     this.bar.drawRoundedRect(
       -4,
@@ -43,27 +41,32 @@ class Hitpoints extends PIXI.Container {
     );
     this.bar.endFill();
 
-    this.bar.beginFill(0xff000099);
+    this.bar.beginFill(0xff0000);
     this.bar.drawRoundedRect(0, 0, HEALTHBAR_WIDTH, HEALTHBAR_HEIGHT, 8);
     this.bar.endFill();
 
-    this.bar.beginFill(0x00ff00);
-    this.bar.drawRoundedRect(
+    this.addChild(this.bar);
+    this.addChild(this.text);
+    this.addChild(this.status);
+  }
+
+  draw(hp, hpPercent) {
+    this.text.text = toFixedRound(hp, 0);
+
+    this.status.clear();
+    this.status.beginFill(0x00ff00);
+    this.status.drawRoundedRect(
       0,
       HEALTHBAR_HEIGHT * (1 - hpPercent),
       HEALTHBAR_WIDTH,
       HEALTHBAR_HEIGHT * hpPercent,
       8
     );
-    this.bar.endFill();
+    this.status.endFill();
   }
 
   update(delta, parent) {
     this.draw(parent.hitpoints, parent.hitpoints / parent.maxHitpoints);
-
-    for (const child of this.children) {
-      if (child.update) child.update(delta, this);
-    }
   }
 }
 
