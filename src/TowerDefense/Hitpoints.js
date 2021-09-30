7905ce82e398124cbe02421258d68d4f7701018b
Jakub Kotrs <jakub.kotrs@emplifi.io>
2021-09-30 13:14:54 +0200

    Redesign hp bar

--- src/TowerDefense/Hitpoints.js
+++ src/TowerDefense/Hitpoints.js
@@ -1,62 +1,65 @@
 import * as PIXI from "pixi.js";
 import { toFixedRound } from "../utils";
 
-const HEALTHBAR_WIDTH = 70;
-const HEALTHBAR_HEIGHT = 10;
+const HEALTHBAR_WIDTH = 10;
+const HEALTHBAR_HEIGHT = 70;
 
 class Hitpoints extends PIXI.Container {
   constructor(hitpoints, maxHitpoints) {
     super();
 
-    this.x = 0;
-    this.y = -20;
+    this.x = 65;
+    this.y = 0;
+    this.alpha = 0.75;
 
     this.text = new PIXI.Text(toFixedRound(hitpoints, 0), {
+      align: "center",
       fontFamily: "Arial",
       fill: ["#00F0F0"],
-      fontSize: 35,
+      fontSize: 20,
       fontWeight: "bold",
       lineJoin: "round"
     });
-    this.text.x = HEALTHBAR_WIDTH + 10;
-    this.text.y = -15;
+    this.text.anchor.set(0.5, 0.5);
+    this.text.x = 0;
+    this.text.y = -20;
 
     this.bar = new PIXI.Graphics();
-
-    this.bar.beginFill(0xff0000);
-    this.bar.drawRect(0, 0, HEALTHBAR_WIDTH, HEALTHBAR_HEIGHT);
-    this.bar.endFill();
-
-    this.bar.beginFill(0x00ff00);
-    this.bar.drawRect(
-      0,
-      0,
-      (HEALTHBAR_WIDTH * hitpoints) / maxHitpoints,
-      HEALTHBAR_HEIGHT
-    );
-    this.bar.endFill();
-
     this.addChild(this.bar);
     this.addChild(this.text);
   }
 
-  update(delta, parent) {
-    this.text.text = toFixedRound(parent.hitpoints, 0);
+  draw(hp, hpPercent) {
+    this.text.text = toFixedRound(hp, 0);
 
     this.bar.clear();
+    this.bar.beginFill(0xffffff);
+    this.bar.drawRoundedRect(
+      -4,
+      -4,
+      HEALTHBAR_WIDTH + 8,
+      HEALTHBAR_HEIGHT + 8,
+      16
+    );
+    this.bar.endFill();
 
-    this.bar.beginFill(0xff0000);
-    this.bar.drawRect(0, 0, HEALTHBAR_WIDTH, HEALTHBAR_HEIGHT);
+    this.bar.beginFill(0xff000099);
+    this.bar.drawRoundedRect(0, 0, HEALTHBAR_WIDTH, HEALTHBAR_HEIGHT, 8);
     this.bar.endFill();
 
     this.bar.beginFill(0x00ff00);
-    this.bar.drawRect(
+    this.bar.drawRoundedRect(
       0,
-      0,
-      (HEALTHBAR_WIDTH * parent.hitpoints) / parent.maxHitpoints,
-      HEALTHBAR_HEIGHT
+      HEALTHBAR_HEIGHT * (1 - hpPercent),
+      HEALTHBAR_WIDTH,
+      HEALTHBAR_HEIGHT * hpPercent,
+      8
     );
     this.bar.endFill();
+  }
+
+  update(delta, parent) {
+    this.draw(parent.hitpoints, parent.hitpoints / parent.maxHitpoints);
 
     for (const child of this.children) {
       if (child.update) child.update(delta, this);
