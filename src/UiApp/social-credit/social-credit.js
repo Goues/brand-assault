7748b2b4afec62db561edf631f9bc39561f06a57
Ales Menzel <ales.menzel@socialbakers.com>
2021-09-30 17:06:42 +0200

    ui: engage - click on the social credit score to gain a little score

--- src/UiApp/social-credit/social-credit.js
+++ src/UiApp/social-credit/social-credit.js
@@ -1,11 +1,27 @@
+import { ReactComponent as CreditCardIcon } from './credit-card.svg'
 import SocialCreditIcon from '../../IdleApp/Icons/SocialCredit'
 import css from './social-credit.module.css'
+import { useDispatch } from 'react-redux'
+import { addCredits } from '../../credits'
 
 export default function SocialCredit({ credits }) {
+  const dispatch = useDispatch()
+
+  const handleClick = () => {
+    dispatch(addCredits(0.1))
+  }
+
   return (
-    <div className={css.socialCredit}>
-      <SocialCreditIcon />
-      <div className={css.socialCreditText}>Social credit: {credits}</div>
+    <div className={css.socialCreditBox}>
+      <div className={css.socialCredit} onClick={handleClick}>
+        <CreditCardIcon />
+        <div className={css.socialCreditText}>
+          <span className={css.socialCreditText}>Social credit:</span>
+          {credits}
+          <SocialCreditIcon />
+        </div>
+      </div>
+      <div className={css.socialCreditHint}>(Click to gain small amount of credit)</div>
     </div>
   )
 }
