c1207fb7e309656b966069c8656b2bde44fadad4
Mehdi Belkihel <mehdi.belkihel@socialbakers.com>
2021-09-30 12:55:55 +0200

    fix: decrease enemy velocity

--- src/TowerDefense/Enemy.js
+++ src/TowerDefense/Enemy.js
@@ -42,7 +42,7 @@ class Enemy extends PIXI.Sprite {
     this.width = SIZE[type];
     this.height = SIZE[type];
     this.nextPathIndex = 0;
-    this.velocity = 0.3; // per 1s
+    this.velocity = 0.1; // per 1s
     this.maxHitpoints = hitpoints;
     this.hitpoints = hitpoints;
     this.interactive = true;
