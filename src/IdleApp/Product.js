34f29e6a3c548fe53eaf8b17579cad6ab7bde47a
Jakub Kotrs <jakub.kotrs@emplifi.io>
2021-09-30 21:57:22 +0200

    Lint

--- src/IdleApp/Product.js
+++ src/IdleApp/Product.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Button from '../Button'
 import { toFixedRound } from '../utils'
