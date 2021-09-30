819c8331b6aaf19ea980727106b8140075df31d6
Jakub Kotrs <jakub.kotrs@emplifi.io>
2021-09-30 09:42:35 +0200

    Fix early return

--- src/TowerDefense/Enemy.js
+++ src/TowerDefense/Enemy.js
@@ -40,7 +40,7 @@ class Enemy extends PIXI.Sprite {
     this.width = SIZE[type];
     this.height = SIZE[type];
     this.nextPathIndex = 0;
-    this.velocity = 0.1; // per 1s
+    this.velocity = 1; // per 1s
     this.hitpoints = hitpoints;
     this.interactive = true;
     this.buttonMode = true;
@@ -88,8 +88,8 @@ class Enemy extends PIXI.Sprite {
         default:
           store.dispatch(subtractCredits(this.hitpoints));
           this.destroy();
-          return;
       }
+      return;
     }
 
     let remainder = this.traveled % TILE_WIDTH;
