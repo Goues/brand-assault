3100634b2c035aa3dab54081d22456b125ff50a8
Ales Menzel <ales.menzel@socialbakers.com>
2021-09-30 17:38:35 +0200

    ui: add tooltip to towers

--- src/UiApp/defend-towers/defend-towers.js
+++ src/UiApp/defend-towers/defend-towers.js
@@ -1,10 +1,21 @@
-import {ReactComponent as DatabaseIcon} from './database.svg';
+import { ReactComponent as DatabaseIcon } from './database.svg'
+import Tippy from '@tippyjs/react'
 
-import css from "./defend-towers.module.css";
+import css from './defend-towers.module.css'
 
-export default function DefendTowers({towers, maxTowers}) {
-    return <div className={css.defendTowers}>
-        <DatabaseIcon />
-        <div class={css.defendTowersText}>Defend towers: {towers}/{maxTowers}</div>
+const Dialog = () => {
+  return <div className={css.dialog}>Built towers / Max towers</div>
+}
+
+export default function DefendTowers({ towers, maxTowers }) {
+  return (
+    <div className={css.defendTowers}>
+      <DatabaseIcon />
+      <Tippy content={<Dialog />}>
+        <div class={css.defendTowersText}>
+          Defend towers: {towers}/{maxTowers}
+        </div>
+      </Tippy>
     </div>
+  )
 }
