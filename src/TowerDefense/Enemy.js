dc920ecc9abed26c049f2a1453c3383d95cad2e2
Jakub Kotrs <jakub.kotrs@emplifi.io>
2021-09-30 09:35:15 +0200

    improve path finding to distance traveled

--- src/TowerDefense/Enemy.js
+++ src/TowerDefense/Enemy.js
@@ -1,10 +1,14 @@
 import * as PIXI from "pixi.js";
-import { PATH, START_TILE, TILE_HEIGHT, TILE_WIDTH } from "./config";
+import {
+  PATH,
+  START_TILE,
+  TILE_HEIGHT,
+  TILE_WIDTH,
+  DIRECTIONS
+} from "./config";
 import store from "../gameState";
 import { addCredits, subtractCredits } from "../credits";
 import Hitpoints from "./Hitpoints";
-
-const PATH_TRESHOLD = 0.1;
 
 const IMAGE = {
   negative: "/comment_negative.png",
@@ -40,7 +44,7 @@ class Enemy extends PIXI.Sprite {
     this.hitpoints = hitpoints;
     this.interactive = true;
     this.buttonMode = true;
-    this.delay = delay * 900 + Math.random() * 200;
+    this.traveled = 0 - (delay * 100 + Math.random() * 50);
 
     this.on("pointerdown", () => {
       this.hit(1);
@@ -61,58 +65,78 @@ class Enemy extends PIXI.Sprite {
   update(delta) {
     if (this.destroyed) return;
 
-    // mimic delay between enemies
-    if (this.delay > 0) {
-      this.delay -= delta;
-      if (this.delay < 0) {
-        delta = this.delay * -1;
-      } else {
-        return;
+    this.traveled += delta * this.velocity;
+
+    if (this.traveled < 0) {
+      return;
+    }
+
+    let pathIndex = Math.floor(this.traveled / TILE_WIDTH);
+
+    if (pathIndex >= PATH.length) {
+      switch (this.type) {
+        case "positive":
+          store.dispatch(addCredits(this.hitpoints));
+          break;
+        case "neutral":
+          this.x = START_TILE.x * TILE_WIDTH + this.offset.x;
+          this.y = (START_TILE.y - 1) * TILE_HEIGHT + this.offset.y;
+          this.type = "negative";
+          this.texture = PIXI.Texture.from(IMAGE[this.type]);
+          this.nextPathIndex = 0;
+          break;
+        default:
+          store.dispatch(subtractCredits(this.hitpoints));
+          this.destroy();
+          return;
       }
     }
 
-    let node = PATH[this.nextPathIndex];
-    let nodeX = node.x * TILE_WIDTH + this.offset.x;
-    let nodeY = node.y * TILE_HEIGHT + this.offset.y;
+    let remainder = this.traveled % TILE_WIDTH;
+    let firstHalf = remainder <= TILE_WIDTH / 2;
+    let node = PATH[pathIndex];
+    let x = node.x * TILE_WIDTH + this.offset.x;
+    let y = node.y * TILE_WIDTH + this.offset.y;
 
-    // check if we are already at the next node with a leeway of % of tile
-    if (
-      this.x > nodeX - TILE_WIDTH * PATH_TRESHOLD &&
-      this.x < nodeX + TILE_WIDTH * PATH_TRESHOLD &&
-      this.y > nodeY - TILE_HEIGHT * PATH_TRESHOLD &&
-      this.y < nodeY + TILE_HEIGHT * PATH_TRESHOLD
-    ) {
-      this.nextPathIndex += 1;
-
-      if (this.nextPathIndex >= PATH.length) {
-        switch (this.type) {
-          case "positive":
-            store.dispatch(addCredits(this.hitpoints));
-            break;
-          case "neutral":
-            this.x = START_TILE.x * TILE_WIDTH + this.offset.x;
-            this.y = (START_TILE.y - 1) * TILE_HEIGHT + this.offset.y;
-            this.type = "negative";
-            this.texture = PIXI.Texture.from(IMAGE[this.type]);
-            this.nextPathIndex = 0;
-            break;
-          default:
-            store.dispatch(subtractCredits(this.hitpoints));
-            this.destroy();
-            return;
-        }
+    if (firstHalf) {
+      switch (node.from) {
+        case DIRECTIONS.TOP:
+          y += remainder - TILE_WIDTH / 2;
+          break;
+        case DIRECTIONS.BOTTOM:
+          y -= remainder - TILE_WIDTH / 2;
+          break;
+        case DIRECTIONS.LEFT:
+          x += remainder - TILE_WIDTH / 2;
+          break;
+        case DIRECTIONS.RIGHT:
+          x -= remainder - TILE_WIDTH / 2;
+          break;
+        default:
+          break;
       }
     } else {
-      const dx = nodeX - this.x;
-      const dy = nodeY - this.y;
-      const d = Math.sqrt(dx ** 2 + dy ** 2);
-
-      const normalizedX = dx / d;
-      const normalizedY = dy / d;
-
-      this.x += normalizedX * this.velocity * delta;
-      this.y += normalizedY * this.velocity * delta;
+      remainder = remainder - TILE_WIDTH / 2;
+      switch (node.to) {
+        case DIRECTIONS.TOP:
+          y -= remainder;
+          break;
+        case DIRECTIONS.BOTTOM:
+          y += remainder;
+          break;
+        case DIRECTIONS.LEFT:
+          x -= remainder;
+          break;
+        case DIRECTIONS.RIGHT:
+          x += remainder;
+          break;
+        default:
+          break;
+      }
     }
+
+    this.x = x;
+    this.y = y;
 
     for (const child of this.children) {
       if (child.update) child.update(delta, this);
