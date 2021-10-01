4d784447481ebac8e081ec357206a427d8631ffc
Jakub Kotrs <jakub.kotrs@emplifi.io>
2021-10-01 11:20:58 +0200

    Fix svg props

--- src/IdleApp/Icons/Star.js
+++ src/IdleApp/Icons/Star.js
@@ -4,8 +4,8 @@ const Star = (props, ref) => {
 	return (
 		<svg width='14' height='14' viewBox='0 0 14 14' fill='none' {...props} ref={ref}>
 			<path
-				fill-rule='evenodd'
-				clip-rule='evenodd'
+				fillRule='evenodd'
+				clipRule='evenodd'
 				d='M14 5.33474L8.967 4.87789L7 0L5.033 4.88526L0 5.33474L3.822 8.82L2.674 14L7 11.2516L11.326 14L10.185 8.82L14 5.33474Z'
 				fill='#FBBC05'
 			/>
