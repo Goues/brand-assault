71fe51f6a884b5d2404ed2c2372b87bb10724f41
Jan Pavlovsky <jan.pavlovsky@socialbakers.com>
2021-10-01 13:01:23 +0200

    towers are agents

--- src/UiApp/defend-towers/defend-towers.js
+++ src/UiApp/defend-towers/defend-towers.js
@@ -1,21 +1,21 @@
-import { ReactComponent as DatabaseIcon } from "./database.svg";
-import Tippy from "@tippyjs/react";
+import { ReactComponent as DatabaseIcon } from './database.svg'
+import Tippy from '@tippyjs/react'
 
-import css from "./defend-towers.module.css";
+import css from './defend-towers.module.css'
 
 const Dialog = () => {
-  return <div className={css.dialog}>Built towers / Max towers</div>;
-};
+	return <div className={css.dialog}>Built agents / Max agents</div>
+}
 
 export default function DefendTowers({ towers, maxTowers }) {
-  return (
-    <div className={css.defendTowers}>
-      <DatabaseIcon />
-      <Tippy content={<Dialog />}>
-        <div className={css.defendTowersText}>
-          Defend towers: {towers}/{maxTowers}
-        </div>
-      </Tippy>
-    </div>
-  );
+	return (
+		<div className={css.defendTowers}>
+			<DatabaseIcon />
+			<Tippy content={<Dialog />}>
+				<div className={css.defendTowersText}>
+					Defend agents: {towers}/{maxTowers}
+				</div>
+			</Tippy>
+		</div>
+	)
 }
